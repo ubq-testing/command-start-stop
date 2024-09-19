a4f9ecb40d40378c968753ab748db9ffa6c59ad1
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-09-19 12:01:04 +0100

    Update tests/main.test.ts
    Co-authored-by: Mentlegen <9807008+gentlementlegen@users.noreply.github.com>

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -93,7 +93,7 @@ describe("User start/stop", () => {
   });
 
   test("Stopping an issue should close the author's linked PR", async () => {
-    const infoSpy = jest.spyOn(console, "info").mockImplementation(() => { });
+    const infoSpy = jest.spyOn(console, "info").mockImplementation(() => {});
     const issue = db.issue.findFirst({ where: { id: { equals: 2 } } }) as unknown as Issue;
     const sender = db.users.findFirst({ where: { id: { equals: 2 } } }) as unknown as PayloadSender;
     const context = createContext(issue, sender, "/stop") as Context<"issue_comment.created">;
