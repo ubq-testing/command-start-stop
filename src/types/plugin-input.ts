258b134e7c3ceed562775681d610f4022a765dd0
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-09-19 09:22:39 +0100

    chore: normalize maxTask roles, add test

--- src/types/plugin-input.ts
+++ src/types/plugin-input.ts
@@ -13,12 +13,22 @@ export interface PluginInputs<T extends SupportedEventsU = SupportedEventsU, TU 
 
 function maxConcurrentTasks() {
   return T.Transform(T.Record(T.String(), T.Integer(), { default: { member: 10, contributor: 2 } }))
-    .Decode((value) => {
+    .Decode((obj) => {
+      // normalize the role keys to lowercase
+      obj = Object.keys(obj).reduce(
+        (acc, key) => {
+          acc[key.toLowerCase()] = obj[key];
+          return acc;
+        },
+        {} as Record<string, number>
+      );
+
       // If admin is omitted, defaults to infinity
-      if (!Object.keys(value).includes("admin")) {
-        value["admin"] = Infinity;
+      if (!obj["admin"]) {
+        obj["admin"] = Infinity;
       }
-      return value;
+
+      return obj;
     })
     .Encode((value) => value);
 }
