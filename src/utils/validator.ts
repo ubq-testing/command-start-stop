818e7479d0aed5ea1bab3bab4bc61c5ef7cf6362
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-09-23 20:06:34 +0900

    chore: fixed errors

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,15 +1,14 @@
-import { Value } from "@sinclair/typebox/value";
+import { Value, ValueError } from "@sinclair/typebox/value";
 import { Env, envConfigValidator, startStopSchema, StartStopSettings, startStopSettingsValidator } from "../types";
 
 export function validateAndDecodeSchemas(env: Env, rawSettings: object) {
   const settings = Value.Default(startStopSchema, rawSettings) as StartStopSettings;
 
   if (!startStopSettingsValidator.test(settings)) {
-    const errorDetails: object[] = [];
+    const errorDetails: ValueError[] = [];
     for (const error of startStopSettingsValidator.errors(settings)) {
-      const errorMessage = { path: error.path, message: error.message, value: error.value };
-      console.error(errorMessage);
-      errorDetails.push(errorMessage);
+      console.error(error);
+      errorDetails.push(error);
     }
     return new Response(JSON.stringify({ message: `Bad Request: the settings are invalid.`, errors: errorDetails }), {
       status: 400,
@@ -20,11 +19,10 @@ export function validateAndDecodeSchemas(env: Env, rawSettings: object) {
   const decodedSettings = Value.Decode(startStopSchema, settings);
 
   if (!envConfigValidator.test(env)) {
-    const errorDetails: object[] = [];
+    const errorDetails: ValueError[] = [];
     for (const error of envConfigValidator.errors(env)) {
-      const errorMessage = { path: error.path, message: error.message, value: error.value };
-      console.error(errorMessage);
-      errorDetails.push(errorMessage);
+      console.error(error);
+      errorDetails.push(error);
     }
     return new Response(JSON.stringify({ message: `Bad Request: the environment is invalid.`, errors: errorDetails }), {
       status: 400,
