30959ec39e4e7567a1d4a8df35900dfb0ca410c7
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-09-23 17:39:52 +0900

    feat: pull edit handler

--- src/types/context.ts
+++ src/types/context.ts
@@ -6,7 +6,7 @@ import { createAdapters } from "../adapters";
 import { Env } from "./env";
 import { Logs } from "@ubiquity-dao/ubiquibot-logger";
 
-export type SupportedEventsU = "issue_comment.created" | "issues.assigned" | "pull_request.opened" | "issues.unassigned";
+export type SupportedEventsU = "issue_comment.created" | "issues.assigned" | "pull_request.opened" | "pull_request.edited" | "issues.unassigned";
 
 export type SupportedEvents = {
   [K in SupportedEventsU]: K extends WebhookEventName ? WebhookEvent<K> : never;
