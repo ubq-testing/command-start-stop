f36e4845b8439b7c88f0c665a1c186210229cefa
gentlementlegen <fernand.veyrier@epitech.eu>
2024-11-09 11:43:08 +0900

    chore: merge if statement

--- src/handlers/user-start-stop.ts
+++ src/handlers/user-start-stop.ts
@@ -34,13 +34,11 @@ export async function userSelfAssign(context: Context<"issues.assigned">): Promi
   const deadline = getDeadline(issue.labels);
 
   // We avoid posting a message if the bot is the actor to avoid double posting
-  if (payload.sender.type === "Bot") {
-    context.logger.debug("Skipping deadline posting message because the sender is a bot.");
-    return { status: HttpStatusCode.NOT_MODIFIED };
-  }
-
-  if (!deadline) {
-    context.logger.debug("Skipping deadline posting message because no deadline has been set.");
+  if (!deadline || payload.sender.type === "Bot") {
+    context.logger.debug("Skipping deadline posting message.", {
+      senderType: payload.sender.type,
+      deadline: deadline,
+    });
     return { status: HttpStatusCode.NOT_MODIFIED };
   }
 
